37dfbab40e6e6ae518297046dacd9879b4b078c3
dougfort <doug.fort@gmail.com>
2021-12-14 07:08:18 -0500

    complete basic example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Error;
+
+mod actor;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let handle = actor::MyActorHandle::new();
+    let id = handle.get_unique_id().await;
+    println!("id = {}", id);
+
+    Ok(())
 }
